f37d128eeb69d19759708195b0b8a5064f628f61
F.LuVx <yeyu0926@hotmail.com>
2026-05-20 19:12:06 +0800

    update

--- Casks/openwarp.rb
+++ Casks/openwarp.rb
@@ -1,10 +1,10 @@
 cask "openwarp" do
+  arch arm: "arm64", intel: "intel"
   version "2026.05.19.preview"
   sha256 arm:   "d4abd9d57056c3b90e0044e366ecfb403fbcf7cee45702821983394e8475fbab",
          intel: "64eb6053b8f11e5dadadc658d0dcee467c581eacc76621156c7867cd986ee6c0"
 
-  url "https://github.com/zerx-lab/warp/releases/download/v#{version}/" \
-      "OpenWarp-#{Hardware::CPU.arm? ? "arm64" : "intel"}.dmg"
+  url "https://github.com/zerx-lab/warp/releases/download/v#{version}/OpenWarp-#{arch}.dmg"
 
   name "OpenWarp"
   desc "Warp的开源版本,支持多AI供应商"
@@ -14,6 +14,8 @@ cask "openwarp" do
     url :url
     strategy :github_latest
   end
+
+  auto_updates true
 
   app "OpenWarp.app"
 
