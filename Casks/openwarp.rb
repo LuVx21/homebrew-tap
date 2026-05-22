ebdb296693c6a375299cff6ee566fcbd94f7c599
F.LuVx <yeyu0926@hotmail.com>
2026-05-22 15:55:19 +0800

    openwarp -> zap

--- Casks/openwarp.rb
+++ Casks/openwarp.rb
@@ -1,12 +1,12 @@
-cask "openwarp" do
+cask "zap" do
   arch arm: "arm64", intel: "intel"
   version "2026.05.20.preview"
   sha256 arm:   "7a49688d56dbf3dece7b7c99bd8b012db8f424a50ad2df470f24ca9d95b1d88e",
          intel: "3211ae26e5452a8d5f79ca1be59e64151af3abaebf9be1b617ebde31e57d10b7"
 
-  url "https://github.com/zerx-lab/zap/releases/download/v#{version}/OpenWarp-#{arch}.dmg"
+  url "https://github.com/zerx-lab/zap/releases/download/v#{version}/Zap-#{arch}.dmg"
 
-  name "OpenWarp"
+  name "Zap"
   desc "Warp的开源版本,支持多AI供应商"
   homepage "https://github.com/zerx-lab/zap"
 
@@ -17,10 +17,10 @@ cask "openwarp" do
 
   auto_updates true
 
-  app "OpenWarp.app"
+  app "Zap.app"
 
   zap trash: [
-    "~/Library/Preferences/dev.openwarp.OpenWarp.plist",
-    "~/Library/Application Support/dev.openwarp.OpenWarp",
+    "~/Library/Preferences/dev.zap.Zap.plist",
+    "~/Library/Application Support/dev.zap.Zap",
   ]
 end
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
 Actions自动更新:
-| rb                   | APP                   | Apple Silicon      | Intel              |
-| :------------------- | :-------------------- | :----------------- | :----------------- |
-| Casks/clashx-meta.rb | MetaCubeX/ClashX.Meta | ClashX Meta.zip    |                    |
-| Casks/openwarp.rb    | zerx-lab/zap          | OpenWarp-arm64.dmg | OpenWarp-intel.dmg |
+| rb                   | APP                   | Apple Silicon   | Intel         |
+| :------------------- | :-------------------- | :-------------- | :------------ |
+| Casks/clashx-meta.rb | MetaCubeX/ClashX.Meta | ClashX Meta.zip |               |
+| Casks/zap.rb         | zerx-lab/zap          | Zap-arm64.dmg   | Zap-intel.dmg |
 
 
 非自动更新:
