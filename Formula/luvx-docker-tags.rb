b81696359032b4e81a9e863463698c47708b5db8
F.LuVx <yeyu0926@hotmail.com>
2026-06-02 19:03:42 +0800

    update

---
+++ Formula/luvx-docker-tags.rb
@@ -0,0 +1,18 @@
+class "luvx-docker-tags < Formula
+  url "https://github.com/LuVx21/coding-go.git",
+      revision: "2d7b9a81f3994e06b02fa80f5709d6dbf5d86dc9"
+  version "0.0.1"
+
+  desc "golang 实现的查询镜像的tag"
+  homepage "https://github.com/LuVx21/coding-go"
+
+  unless which("go")
+    depends_on "go" => :build
+  end
+
+  def install
+    ENV["GO111MODULE"] = "on"
+    ENV["GOPROXY"] = "https://goproxy.cn,direct" if OS.mac?
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "-o", bin/"docker-tags", "./cmd/docker"
+  end
+end
---
+++ Formula/luvx-kkv.rb
@@ -0,0 +1,12 @@
+class "luvx-kkv" < Formula
+  url "https://github.com/LuVx21/shell.git",
+      revision: "512a9bd9875c05e2ec69a01716d470a34c8d889e"
+  version "0.0.1"
+
+  desc "shell 实现的简易本地kv存储"
+  homepage "https://github.com/LuVx21/shell"
+
+  def install
+    bin.install "bin/source/script/kv.sh" => "kkv"
+  end
+end
