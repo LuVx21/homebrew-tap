858615c77d9a5cb1f0f1a7c4fee4d92357d51381
F.LuVx <yeyu0926@hotmail.com>
2026-08-06 13:34:41 +0800

    update

--- Casks/macoptimizer.rb
+++ Casks/macoptimizer.rb
@@ -1,8 +1,8 @@
 cask "macoptimizer" do
   arch arm: "AppleSilicon", intel: "Intel"
   version "5.0"
-  sha256 arm:   "",
-         intel: ""
+  sha256 arm:   "68ae036bef1418c37ef0522c7cdc7f1d37978c1f7f9487306c4bddeda4122403",
+         intel: "4f84d5b51bcffb6bdd9bb62cbe915f9f141b88aa4785cef0f3e4aa7f659e1321"
 
   url "https://github.com/ddlmanus/MacOptimizer/releases/download/v#{version}/MacOptimizer-v#{version}-#{arch}.dmg"
   name "MacOptimizer"
