4211197fa90c0b0e9ab3acca07c9ef0f590764d8
F.LuVx <yeyu0926@hotmail.com>
2026-09-16 16:02:36 +0800

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,12 +2,23 @@
 pwd
 github="raw.githubusercontent.com"
 
+# 非官方
 cask_urls=(
     "https://${github}/lifedever/homebrew-tap/refs/heads/main/Casks/pastememo.rb"
     "https://${github}/lifedever/homebrew-tap/refs/heads/main/Casks/task-tick.rb"
 )
 
+# 常用
+cask_urls+=(
+    "https://${github}/Homebrew/homebrew-cask/refs/heads/main/Casks/d/dropbox.rb"
+)
+
+# 非官方
 formula_urls=(
+
+)
+# 常用
+formula_urls+=(
 
 )
 
